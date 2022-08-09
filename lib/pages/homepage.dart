8a13de73bdb0d1649e8ff313b8a00e6e4ecf8fcd
Isaiah Tan <108507316+IsaiahXingWei@users.noreply.github.com>
2022-08-09 11:52:30 +0800

    Added functionality to navbar

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:harry_potter_mobdev_hackathon/pages/about_us.dart';
 
+import 'bottom_navbar.dart';
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
@@ -12,22 +14,24 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Harry potter Almanac'),),
-      body: Center(
-        child: Column(
-          children: [
-            ElevatedButton(
-                onPressed: (){
-                  Navigator.push(
-                    context, MaterialPageRoute(builder: (context) =>
-                  AboutUs())
-                  );
-                },
-                child: Text("Test"))
-          ],
+        appBar: AppBar(
+          title: const Text('Harry potter Almanac'),
         ),
-      )
-    );
+        body: Center(
+          child: Column(
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const AboutUs()));
+                  },
+                  child: const Text("Test"))
+            ],
+          ),
+        ),
+    bottomNavigationBar: Navbar(isHome: true, isAboutUs: false,),);
+
   }
 }
