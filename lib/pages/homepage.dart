cf22c8cfb1bacb5c75530017cf5ac55f4be64420
Isaiah Tan <108507316+IsaiahXingWei@users.noreply.github.com>
2022-08-10 12:19:36 +0800

    decorated fonts and images

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:harry_potter_mobdev_hackathon/fonts/akaya_teliviga.dart';
 import 'package:harry_potter_mobdev_hackathon/pages/about_us.dart';
 import 'package:harry_potter_mobdev_hackathon/pages/bottom_navbar.dart';
 
@@ -18,94 +19,91 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Harry Potter Almanac'),
+        title: const Text('Harry Potter Almanac'),
       ),
-      body: SingleChildScrollView(
-        child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-          const SizedBox(
-            width: 20.0,
-            height: 30.0,
-          ),
-          Text(
-            'Welcome to Harry Potter Wizardly App',
-            style: TextStyle(
-                color: Colors.red,
-                fontSize: 23.0,
-                fontFamily: 'Akaya Telivigala'),
-          ),
-          const SizedBox(
-            width: 20.0,
-            height: 20.0,
-          ),
-          Container(
-            padding: EdgeInsets.all(20.0),
-            child: Column(
-              children: [
-                Text(
-                  'Get to know to your favourite Harry Potter characters here in our app. See below & choose your favourite Harry Potter houses! ',
-                  style: TextStyle(
-                      color: Colors.red,
-                      fontSize: 18.0,
-                      fontFamily: 'Akaya Telivigala'),
-                ),
-                const SizedBox(
-                  width: 60.0,
-                  height: 60.0,
-                ),
-                Wrap(children: [
-                  const Image(
-                    image: NetworkImage(
-                        'https://1000logos.net/wp-content/uploads/2021/11/Gryffindor-Logo.png'),
+      body: Container(
+        constraints: BoxConstraints.expand(),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/images/background.jpeg"),
+              fit: BoxFit.cover),
+        ),
+        child: SingleChildScrollView(
+          child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+            const SizedBox(
+              width: 20.0,
+              height: 30.0,
+            ),
+            const Text(
+              'Welcome to Harry Potter Wizardly App',
+              style: AkayaTelivigala(color: Colors.red, size: 23,)
+            ),
+            const SizedBox(
+              width: 20.0,
+              height: 20.0,
+            ),
+            Container(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  const Text(
+                    'Get to know to your favourite Harry Potter characters here in our app. See below & choose your favourite Harry Potter houses! ',
+                    style: AkayaTelivigala(color: Colors.red, size: 18,),
                   ),
-
-                  const SizedBox(
-                    width: 20.0,
-                    height: 20.0,
-                  ),
-
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {},
-                        child: const Text('Visit Gryffindor'),
-                      ),
-                    ],
-                  ),
-
                   const SizedBox(
                     width: 60.0,
                     height: 60.0,
                   ),
-
-                  const Image(
-                    image: NetworkImage(
-                        'https://www.logolynx.com/images/logolynx/53/5390e974544de6279c4d9cb6253e3a2c.jpeg'),
-                  ),
-
-                  const SizedBox(
-                    width: 20.0,
-                    height: 20.0,
-                  ),
-
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {},
-                        child: const Text('Visit Slytherin'),
-                      ),
-                    ],
-                  ),
-
-
-                ]),
-              ],
-            ),
-          )
-        ]),
+                  Wrap(children: [
+                    const Image(
+                      image: NetworkImage(
+                          'https://1000logos.net/wp-content/uploads/2021/11/Gryffindor-Logo.png'),
+                    ),
+                    const SizedBox(
+                      width: 20.0,
+                      height: 20.0,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: const Text('Visit Gryffindor'),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      width: 60.0,
+                      height: 60.0,
+                    ),
+                    const Image(
+                      image: NetworkImage(
+                          'https://www.logolynx.com/images/logolynx/53/5390e974544de6279c4d9cb6253e3a2c.jpeg'),
+                    ),
+                    const SizedBox(
+                      width: 20.0,
+                      height: 20.0,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: const Text('Visit Slytherin'),
+                        ),
+                      ],
+                    ),
+                  ]),
+                ],
+              ),
+            )
+          ]),
+        ),
       ),
-      bottomNavigationBar: Navbar(isHome: true, isAboutUs: false,),
+      bottomNavigationBar: Navbar(
+        isHome: true,
+        isAboutUs: false,
+      ),
     );
   }
 }
