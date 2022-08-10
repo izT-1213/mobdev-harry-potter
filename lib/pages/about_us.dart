4d5985191ee35d74551785886beaa3e2be2e0446
Isaiah Tan <108507316+IsaiahXingWei@users.noreply.github.com>
2022-08-10 17:45:57 +0800

    Minor refactoring

--- lib/pages/about_us.dart
+++ lib/pages/about_us.dart
@@ -8,10 +8,19 @@ class AboutUs extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const String story =
+        "T'was a cold and rainy night when three delinquents are lost in the woods, having dared defy their masters and brave the Forest.  \n\n"
+        "They were taking shelter beneath a tree when a howl suddenly rang out behind them. The trio turned quickly to glimpse a pack of werewolves nearby and a masked horseman riding close beside. An order was given, and the wolves gave chase. Terrified, they ran aimlessly away from their pursuers, fear and adrenaline both willing them to hasten their steps. Still, alas, they could not outrun the inevitable.   \n\n"
+        "The attackers encircled the helpless trio and presented them as captives before their master, Lord Voldemort, to whom he saw befitting that the three served the rest of their miserable lives developing and maintaining a mobile app to promote and magnify the greatness of Hogwarts to the rest of the world. \n\n"
+        "And the trio are:";
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text("About Us"),
-        centerTitle: true,
+        title: const Text(
+          "About Us",
+          style: AkayaTelivigala(
+              color: Colors.white, size: 25, fontWeight: FontWeight.bold),
+        ),
       ),
       body: Container(
         constraints: BoxConstraints.expand(),
@@ -20,102 +29,130 @@ class AboutUs extends StatelessWidget {
               image: AssetImage("assets/images/background.jpeg"),
               fit: BoxFit.cover),
         ),
-        child: Column(
-          // mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Center(
-              child: Text("smthg smthg yadda yadda bla bla bla",
-                style: AkayaTelivigala(color: Colors.red, size: 23,),),
-            ),
-            const SizedBox(
-              height: 50,
-            ), //Just for space between two rows
-            Column(
-              // mainAxisAlignment: MainAxisAlignment.spaceAround,
-              // crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Image.asset(
-                        'assets/images/rn_tea.jpeg',
-                        width: 100.0,
-                        height: 100.0,
-                      ),
-                    ),
-                    Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text(
-                          'Nurul Izzati',
-                          style: AkayaTelivigala(color: Colors.red, size: 28,),
+        child: SingleChildScrollView(
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                story,
+                style: AkayaTelivigala(
+                  color: Colors.black,
+                  size: 23,
+                ),
+                textAlign: TextAlign.justify,
+              ),
+              const Divider(
+                color: Colors.black,
+                height: 30,
+                thickness: 3,
+              ),
+              Column(
+                // mainAxisAlignment: MainAxisAlignment.spaceAround,
+                // crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Image.asset(
+                          'assets/images/rn_tea.jpeg',
+                          width: 100.0,
+                          height: 100.0,
                         ),
-                        Text('Our favourite streamerrrrrrrrr xD',
-                          style: AkayaTelivigala(color: Colors.black, size: 18,),),
-                      ],
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                Row(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.all(8.0),
+                      ),
+                      Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            'Nurul Izzati',
+                            style: AkayaTelivigala(
+                              color: Colors.red,
+                              size: 28,
+                            ),
+                          ),
+                          Text(
+                            'Our favourite streamerrrrrrrrr xD',
+                            style: AkayaTelivigala(
+                              color: Colors.black,
+                              size: 18,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
                         child: Image.asset(
                           'assets/images/jazli.jpeg',
                           width: 100.0,
                           height: 100.0,
                         ),
-                    ),
-                    Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text(
-                          'Jazli',
-                          style: AkayaTelivigala(color: Colors.red, size: 28,),
-                        ),
-                        Text('Yes, this is his real face.',
-                            style: AkayaTelivigala(color: Colors.black, size: 18,)),
-                      ],
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                Row(
-                  children: [
-                     Padding(
-                      padding: EdgeInsets.all(8.0),
+                      ),
+                      Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            'Jazli',
+                            style: AkayaTelivigala(
+                              color: Colors.red,
+                              size: 28,
+                            ),
+                          ),
+                          Text('Yes, this is his real face.',
+                              style: AkayaTelivigala(
+                                color: Colors.black,
+                                size: 18,
+                              )),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
                         child: Image.asset(
                           'assets/images/waifu.jpeg',
                           width: 100.0,
                           height: 100.0,
                         ),
-                    ),
-                    Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text(
-                          'Isaiah Tan',
-                          style: AkayaTelivigala(color: Colors.red, size: 28,),
-                        ),
-                        Text("He's kinda cringe, ngl.",
-                            style: AkayaTelivigala(color: Colors.black, size: 18,)),
-                      ],
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ],
+                      ),
+                      Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            'Isaiah Tan',
+                            style: AkayaTelivigala(
+                              color: Colors.red,
+                              size: 28,
+                            ),
+                          ),
+                          Text("He's kinda cringe, ngl.",
+                              style: AkayaTelivigala(
+                                color: Colors.black,
+                                size: 18,
+                              )),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: Navbar(
