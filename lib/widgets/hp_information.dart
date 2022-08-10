09ad092aabc5b3ca677eb37ff3bca4a6ff7e1742
Nurul Izzati <izzati@besquare.com.my>
2022-08-10 15:38:20 +0800

    Character page edited

--- lib/widgets/hp_information.dart
+++ lib/widgets/hp_information.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 import '../models/harrypotter_model.dart';
 
@@ -10,27 +11,30 @@ class HPInformation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView.count(
-        primary: false,
-        padding: const EdgeInsets.all(10),
-        crossAxisCount: 2,
-        mainAxisSpacing: 10,
-        crossAxisSpacing: 10,
-        children: [
-      for(int i = 0; i < hpCharacters.length; i++) Container(
-        alignment: Alignment.bottomCenter,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15),
-          image: DecorationImage(
-            fit: BoxFit.cover,
-            image: NetworkImage(hpCharacters[i].image),
+      primary: false,
+      padding: const EdgeInsets.all(10),
+      crossAxisCount: 2,
+      mainAxisSpacing: 10,
+      crossAxisSpacing: 10,
+      children: [
+        for (int i = 0; i < hpCharacters.length; i++)
+          Card(
+            elevation: 10,
+            child: ClipRRect (
+              borderRadius: BorderRadius.circular(8.0),
+              child: hpCharacters[i].image.isNotEmpty
+                  ? CachedNetworkImage(
+                    imageUrl: hpCharacters[i].image,
+                    placeholder: (context, url) => CircularProgressIndicator(),
+                fit: BoxFit.fitWidth,
+              )
+                  : CachedNetworkImage(
+                imageUrl:
+                'https://www.seekpng.com/png/detail/966-9665317_placeholder-image-person-jpg.png',
+              ),
+            ),
           )
-        ),
-        child: Text(hpCharacters[i].name,
-        style: const TextStyle(
-          color: Colors.white,
-          fontSize: 18,
-        )),
-      )
-    ]);
+      ],
+    );
   }
 }
