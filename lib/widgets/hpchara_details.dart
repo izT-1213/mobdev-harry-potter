6e2f645d80ddfd576a42e137508e77c699236050
Isaiah Tan <108507316+IsaiahXingWei@users.noreply.github.com>
2022-08-11 10:18:11 +0800

    Added student and staff empty page, and navbar functionality

--- lib/widgets/hpchara_details.dart
+++ lib/widgets/hpchara_details.dart
@@ -30,55 +30,63 @@ class CharacterProfile extends StatelessWidget {
       appBar: AppBar(
         title: Text(name),
       ),
-      body: Padding(padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-            child: Column(
-              children: [
-                Container(
-                  height: 210,
-                  width: 170,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12),
-                    image: DecorationImage(
-                    image: NetworkImage(image),
-                    fit: BoxFit.cover))),
-                const SizedBox(height: 15,),
-                Text(name,
-                    style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
-                    textAlign: TextAlign.left,),
-                const SizedBox(height: 15,),
-                Container(
-                  alignment: Alignment.centerLeft,
-                  child: Row(
-                    children: [
-                      const Text("House :", style: TextStyle(
-                        fontSize: 18,
-                          fontWeight: FontWeight.bold),),
-                      const SizedBox(width: 25,),
-                      Text(house, style: const TextStyle(
-                        fontSize: 18
-                      ),),
-                    ]
-                  ),
-                  ),
-                const SizedBox(height: 8,),
-                Container(
-                  alignment: Alignment.centerLeft,
-                  child: Row(
-                    children: [
-                      const Text("Species :", style: TextStyle(
+      body: Container(
+        constraints: BoxConstraints.expand(),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/images/background.jpeg"),
+              fit: BoxFit.cover),
+        ),
+        child: Padding(padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+              child: Column(
+                children: [
+                  Container(
+                    height: 210,
+                    width: 170,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      image: DecorationImage(
+                      image: NetworkImage(image),
+                      fit: BoxFit.cover))),
+                  const SizedBox(height: 15,),
+                  Text(name,
+                      style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                      textAlign: TextAlign.left,),
+                  const SizedBox(height: 15,),
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    child: Row(
+                      children: [
+                        const Text("House :", style: TextStyle(
                           fontSize: 18,
-                          fontWeight: FontWeight.bold),),
-                      const SizedBox(width: 25,),
-                      Text(species, style: const TextStyle(
+                            fontWeight: FontWeight.bold),),
+                        const SizedBox(width: 25,),
+                        Text(house, style: const TextStyle(
                           fontSize: 18
-                      ),),
-                    ],
+                        ),),
+                      ]
+                    ),
+                    ),
+                  const SizedBox(height: 8,),
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    child: Row(
+                      children: [
+                        const Text("Species :", style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold),),
+                        const SizedBox(width: 25,),
+                        Text(species, style: const TextStyle(
+                            fontSize: 18
+                        ),),
+                      ],
+                    ),
                   ),
-                ),
-                Text(description),
-          ],
-          )
-          ),
+                  Text(description),
+            ],
+            )
+            ),
+      ),
       );
   }
 }
